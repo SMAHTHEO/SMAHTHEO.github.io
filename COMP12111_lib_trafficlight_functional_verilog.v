afae31a4edf11d93575acf2bc9883e67b3ba242a
SMAHTHEO <114228371+SMAHTHEO@users.noreply.github.com>
2022-11-21 17:02:55 +0000

    Update COMP12111_lib_trafficlight_functional_verilog.v

--- COMP12111_lib_trafficlight_functional_verilog.v
+++ COMP12111_lib_trafficlight_functional_verilog.v
@@ -1,5 +1,4 @@
-// Verilog HDL for "COMP12111", "trafficlight" "functional"
-//
+
 // COMP12111 - Exercise 3 Sequential Design
 //
 // Version 1. Feb 2019. P W Nutter
@@ -27,54 +26,51 @@ module trafficlight ( output reg [4:0] 	lightseq,	//the 5-bit light sequence
 
 
 // declare internal variables here (how many bits required?)
-
 reg [3:0] state, next_statae;	// internal state variables
 
 
 // implement your next state combinatorial logic block here
 
 always@ (*)
+begin
 	case(state)
 		4'b0000:	if(start)	next_statae = 4'b0001;	//s0
 					else		next_statae = 4'b0000;
 
-		4'b0001:	next_statae = 4'b0002;		//s1
-		4'b0010:	next_statae = 4'b0003;		//s2
-		4'b0011:	next_statae = 4'b0004;		//s3
-		4'b0100:	next_statae = 4'b0005;		//s4
+		4'b0001:	next_statae = 4'b0010;		//s1
+		4'b0010:	next_statae = 4'b0011;		//s2
+		4'b0011:	next_statae = 4'b0100;		//s3
+		4'b0100:	next_statae = 4'b0101;		//s4
 
 		4'b0101:	if(start)	next_statae = 4'b1000;	//s5
 					else		next_statae = 4'b0110;
 		4'b0110:	if(start)	next_statae = 4'b1001;	//s6
 					else		next_statae = 4'b0111;
 		4'b0111:	if(start)	next_statae = 4'b1010;	//s7
-					else		next_statae = 4'b1000;
+					else		next_statae = 4'b0000;
 
 		4'b1000:	next_statae = 4'b1001;		//s8
 		4'b1001:	next_statae = 4'b1010;		//s9
 		4'b1010:	next_statae = 4'b0001;		//s10
-
-		
-
 	endcase
+	if (reset) next_statae = 4'b0000;
 end
-
 
 // implement your current state assignment, register, here
-always@ (posedge clock)
-	state <= next_statae
-end
 
+always@ (posedge clock)
+	state <= next_statae;	//save state
 
 // implement your output logic here
+
 
 always@ (*)
 	case(state)
 		4'b0000:	lightseq = 5'b01001;		//s0
-		4'b0001:	lightseq = 5'b10100;		//s1
+		4'b0001:	lightseq = 5'b10010;		//s1
 		4'b0010:	lightseq = 5'b10100;		//s2
 		4'b0011:	lightseq = 5'b10100;		//s3
-		4'b0100:	lightseq = 5'b01001;		//s4
+		4'b0100:	lightseq = 5'b01100;		//s4
 		4'b0101:	lightseq = 5'b01110;		//s5
 		4'b0110:	lightseq = 5'b01001;		//s6
 		4'b0111:	lightseq = 5'b01001;		//s7
@@ -82,6 +78,7 @@ always@ (*)
 		4'b1001:	lightseq = 5'b01001;		//s9
 		4'b1010:	lightseq = 5'b01001;		//s10
 	endcase
+
 
 
 endmodule
